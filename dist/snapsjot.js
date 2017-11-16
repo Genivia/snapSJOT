7696e9a8297b0d754125507e5d6b9c3e9bbb886e
Robert van Engelen <ravengelen@gmail.com>
2017-11-16 09:41:22 -0500

    1.4.0

--- dist/snapsjot.js
+++ dist/snapsjot.js
@@ -2,7 +2,7 @@
  * Create SJOT schema from JSON data
  *
  * @module      snapsjot
- * @version     1.3.18
+ * @version     1.4.0
  * @class       snapSJOT
  * @author      Robert van Engelen, engelen@genivia.com
  * @copyright   Robert van Engelen, Genivia Inc, 2016-2017. All Rights Reserved.
