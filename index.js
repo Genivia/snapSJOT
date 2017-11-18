866c18ac7ec4593d46dd5231a0ffb54dc5f9c152
Robert van Engelen <ravengelen@gmail.com>
2017-11-18 09:59:51 -0500

    Released 1.4.1
    Minor updates

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Create SJOT schema from JSON data
  *
  * @module      snapsjot
- * @version     1.4.0
+ * @version     1.4.1
  * @class       snapSJOT
  * @author      Robert van Engelen, engelen@genivia.com
  * @copyright   Robert van Engelen, Genivia Inc, 2016-2017. All Rights Reserved.
